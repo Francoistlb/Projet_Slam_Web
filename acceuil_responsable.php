36bbec461a07bcd9ca67a15af78ef6a6e7c1a4fc
Francoistlb <113359189+Francoistlb@users.noreply.github.com>
2022-11-21 15:46:52 +0100

    Base de l'appli web

---
+++ acceuil_responsable.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Appli-Frais</title>
+  <link href="https://fonts.googleapis.com/css?family=Karla:400,700&display=swap" rel="stylesheet">
+  <link rel="stylesheet" href="https://cdn.materialdesignicons.com/4.8.95/css/materialdesignicons.min.css">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+  <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="assets/css/login.css">
+</head>
+
+<body>
+  <main class="min-vh-100 py-3 py-md-0">
+    <div class="centrage">
+      <div class="container2">
+        <div class="card login-card">
+          <div class="row no-gutters">
+
+            <div class="img col-md-5">
+              <img src="assets/images/logo_gsb.jpg" alt="login" class="login-card-img">
+            </div>
+
+            <div class="centrage col-md-7">
+              <div class="card-body text-center">
+               <!-- Ouverture de la session --> 
+              <?php
+                  session_start();
+
+                  if (!isset ($_SESSION['email']) && $_SESSION['role'] !=3)
+                     {
+                        echo "Vous n'êtes pas connecté";
+                     } else {
+                        $email=$_SESSION['email'];
+                        echo "Bonjour $email <br> <br>";
+                        echo "Profil responsable <br> <br>";
+                        echo "<a href='kill_session.php'> Déconnexion </a>" ;
+
+                        
+                     }
+                  ?>
+              </div>
+            </div>
+
+          </div>
+        </div>
+      </div>
+      <div class="contenu">
+        <div class="ligne"></div>
+        <div>
+          <h1 class=" text-center"> Saisir le code ci-dessous </h1>      
+        </div>
+      </div>
+    </div>
+  </main>
+  
+</body>
+</html>
+
---
+++ connexion_session.php
@@ -0,0 +1,91 @@
+<?php
+
+ob_start();
+session_start();
+
+if (!isset ($_POST['Envoyer']) && empty($_POST['Envoyer']) && empty($_POST['email']) && empty($_POST['password']))
+   {
+      echo "Veuillez saisir vos identifiants  <br> <br>";
+   } 
+   else
+   {  
+      // Etape 1 : Connexion à la basse de données
+      require_once "connexion_base.php";
+
+      // Etape 2 : Récupération des données du formulaire
+      htmlspecialchars($id=$_POST['email']); // éviter des injections XSS
+      htmlspecialchars($mdp=$_POST['password']);
+      
+      // Etape 3 : Requête de connexion : contrôle de l'email et récupération du mdp hash
+      // Récupération des données dans la table utilisateur
+      $log="SELECT * FROM utilisateur WHERE Mail_utilisateur ='$id'";
+      $req= $bdd->prepare($log);
+      $req->execute();
+      
+      // Stockage des réponses dans la variable $login
+      $login=$req->fetch(); 
+    
+               //var_dump($login);                   // Affiche le résultat de la variable Login issue du fetch
+               //echo '<br><br>'.$login['ID_role'];  // Affiche le résultat de $login['ID_role']
+
+      // Vérifie l'existance du login (si $count trouve 0, il n'y a pas de login)
+      $count=$req->rowCount();
+               //echo $count;
+
+      // Etape 4 : Vérification de l'email. Si >= 0 alors le login éxiste
+      if($count>0)
+      {
+         // Etape 5 :Comparaison du mdp saisi et hashé 
+         if(password_verify($mdp, $login['Passwordd'])) 
+         {
+            // Etape 6 : Ouverture de session
+            $_SESSION['email'] = $id;
+            $_SESSION['role'] = $login['ID_role'];
+      
+            // Etape 7 : Ouverture de l'interface selon le rôle
+            switch ($login['ID_role']) 
+            {
+               case 0:
+                  header('location: Acceuil_visiteur.php'); // Ouvre l'interface pour le visiteur
+                  exit(); 
+               break;
+
+               case 1:
+                  header('location: Acceuil_comptabilite.php'); // Ouvre l'interface pour le comptable
+                  exit();  
+               break;
+
+               case 2:
+                  header('location: Acceuil_secretaire.php'); // Ouvre l'interface pour le responsable visiteur
+                  exit(); 
+               break;
+
+               case 3:
+                  header('location: Acceuil_responsable.php'); // Ouvre l'interface pour l'administrateur
+                  exit(); 
+               break;
+
+               case 4:
+
+                  header('location: Acceuil_administrateur.php'); // Ouvre l'interface pour le secretaire
+                  exit(); 
+               break;
+            }
+         }
+         else
+         {
+            // Si le mot de passe est incorrect, redirection sur index.php avec message d'alerte
+            echo "<script language='Javascript'>alert('Le login ou le mot de passe est incorrecte.')</script>";
+            header('Refresh: 0.1; index.php'); 
+            ob_flush();
+         }
+      }
+      else
+      {
+         // Si le login est incorrect, redirection sur index.php
+         echo "<script language='Javascript'>alert('Le login ou le mot de passe est incorrecte.')</script>";
+         header('Refresh: 0.1; index.php'); 
+         ob_flush();
+      }
+   }
+?>
